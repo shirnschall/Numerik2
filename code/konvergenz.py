41577f3a9e4dc57f0fed0f80dccea135893d084a
RafaelDorigo <e11819554@student.tuwien.ac.at>
2020-01-19 14:58:38 +0100

    updates

--- code/konvergenz.py
+++ code/konvergenz.py
@@ -7,22 +7,26 @@ Created on Sat Jan 18 18:53:53 2020
 
 import numpy as np
 import matplotlib.pylab as plt
+from matplotlib import rc
+
+#rc('text', usetex = True)
 
 def f(k, i):
     return ((np.sqrt(k)-1)/(np.sqrt(k)+1))**i
 
-k = np.arange(1,100,1)
+t = np.arange(1,50,0.01)
 
 fig = plt.figure()
 ax = plt.subplot(111)
 
-ax.plot(k,f(k,1), label='t=1')
-ax.plot(k,f(k,2), 'r--', label='t=2')
-ax.plot(k,f(k,3), '-.', label='t=3')
-ax.plot(k,f(k,4), ':', label='t=4')
+ax.loglog(t,f(2,t), label='$\kappa = 2$')
+ax.loglog(t,f(10,t), 'r--', label='$\kappa = 10$')
+ax.loglog(t,f(100,t), '-.', label='$\kappa = 100$')
+#ax.loglog(t,f(50,t), ':', label='$\kappa = 50$')
 
 ax.grid(which='major', linewidth=0.5)
-ax.set_xlabel('K', size='large')
-ax.set_ylabel('Bruch', size='large')
+ax.set_xlabel('$\kappa$', size='large')
+ax.set_ylabel('$\omega^t$', size='large')
+plt.title(r'$\omega^{t} := \left(\frac{\sqrt{\kappa} - 1}{\sqrt{\kappa} + 1}\right)^{t}$',fontsize='large')
 ax.legend()
 plt.show
