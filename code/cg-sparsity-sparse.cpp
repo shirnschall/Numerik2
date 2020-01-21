a2d092742075134e993a6c8fbeb7f351d94b4123
Sebastian Hirnschall <sebastian@hirnschall.net>
2020-01-21 20:34:44 +0100

    vergleich cg bei verschieden dicht besetzten matrizen

--- code/cg-sparsity-sparse.cpp
+++ code/cg-sparsity-sparse.cpp
@@ -13,14 +13,29 @@ int main() {
     //csv header
     std::cout << "n\tt\ttype\tdensity\n";
 
-        for (int n = 10; n < 1000; n+=50) {
-            for (double j = 0.00; j < 1; j+=0.2) { // density in %
+        for (int n = 10; n < 2000; n+=10) {
+            for (double j = 1; j <10; j+=2) { // density in %
+                //create random symmetric positive definite matrix A and vector b
+                linag::DenseMatrix<double> A(n, n);
+                linag::Vector<double> b(A.dim().rows);
 
+                A.randSPD(j);
+                b.rand();
+
+                linag::SparseMatrix<double> ASparse(A);
+
+
+                std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+                ASparse.conjugateGradientSolver(b,10e-10);
+                std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+                std::cout << n << '\t' << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "\tsparse\t" << j << std::endl;
+            }
             //create random symmetric positive definite matrix A and vector b
             linag::DenseMatrix<double> A(n, n);
             linag::Vector<double> b(A.dim().rows);
 
-            A.randSPD((int)std::floor(n*j)%2?std::floor(n*j):std::floor(n*j)+1);
+            A.rand();
+            A = A * A.transpose();
             b.rand();
 
             linag::SparseMatrix<double> ASparse(A);
@@ -29,9 +44,9 @@ int main() {
             std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
             ASparse.conjugateGradientSolver(b,10e-10);
             std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-            std::cout << n << '\t' << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "\tsparse\t" << j << std::endl;
+            std::cout << n << '\t' << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "\tsparse\tn" << std::endl;
 
-            }
+
 
         }
 
--- plots/cg-sparsity-sparse.R
+++ plots/cg-sparsity-sparse.R
@@ -2,13 +2,14 @@ library(ggplot2)
 require(scales)
 require(dplyr)
 
-data=read.csv("/Users/shirnschall/Desktop/Numerik2/plots/cg-dense-vs-sparse",header = TRUE ,sep = "\t")
+data=read.csv("C:\\Users\\shirnschall\\Documents\\GitHub\\Numerik2\\plots\\cg-sparsity-sparse",header = TRUE ,sep = "\t")
 
-p <- ggplot(data,aes(x=n,y=t,color=type,group=type))+
-  geom_point(aes(shape = type)) + 
-  geom_path(aes(group = type))+
+p <- ggplot(data,aes(x=n,y=t,color=factor(density),group=factor(density)))+
+  geom_point(aes(shape = factor(density))) + 
+  #geom_path(aes(group = factor(density)))+
+  geom_smooth()+ # argument se=F schaltet konvidenzintervall aus
   theme_bw() +
-  labs(color = "Art der Matrix",group="Art der Matrix",linetype="Art der Matrix",shape="Art der Matrix")+
+  labs(color = "Einträge ungleich null",group="Einträge ungleich null",linetype="Einträge ungleich null",shape="Einträge ungleich null")+
   theme(
     legend.position = c(.03, .97),
     legend.justification = c("left", "top"),
@@ -17,10 +18,9 @@ p <- ggplot(data,aes(x=n,y=t,color=type,group=type))+
   )+
 scale_y_log10()+
   ylab("Zeit [us]") +
-  xlab("Matrix (nxn)")+
-  scale_fill_discrete(labels = c("A", "B"))
+  xlab("Matrix (nxn)")
 
 p
 
 
-ggsave("cg-dense-vs-sparse.png", units="in", width=5, height=4, dpi=300)
+ggsave("cg-sparsity-sparse.png", units="in", width=5, height=4, dpi=300)
